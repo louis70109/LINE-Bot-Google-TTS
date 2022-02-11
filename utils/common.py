ce28ecab1618658a737bf26710ff7c9dc3fcb2e0
louis70109 <louis70109@gmail.com>
2022-02-11 12:55:46 +0800

    force upload m4a to mp3 to google bucket

--- utils/common.py
+++ utils/common.py
@@ -2,66 +2,55 @@ import os
 
 from linebot import LineBotApi
 from google.cloud import speech
+from google.cloud import storage
+
+
+def upload_data_to_gcs(bucket_name, data, target_key):
+    try:
+        client = storage.Client()
+        bucket = client.bucket(bucket_name)
+        bucket.blob(target_key).upload_from_string(data)
+        return bucket.blob(target_key).public_url
+
+    except Exception as e:
+        print(e)
+
+    return None
 
 
 def write_audio_file(message_id):
     line_bot_api = LineBotApi(os.getenv('LINE_CHANNEL_ACCESS_TOKEN'))
     message_content = line_bot_api.get_message_content(message_id)
-    # with open('./abc.mp3', 'wb') as fd:
-    #     for chunk in message_content.iter_content():
-    #         fd.write(chunk)
+    total = b''
+    for chunk in message_content.iter_content():
+        total += chunk
+
+    upload_data_to_gcs(os.getenv('GOOGLE_BUCKET'), total, 'hello.mp3')
+
 
 def google_tts():
+    os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = os.getenv('GOOGLE_APPLICATION_CREDENTIALS')
+    # Instantiates a client
+    client = speech.SpeechClient()
 
-    os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = 'client_service_key.json'
-    speech_client = speech.SpeechClient()
+    # The name of the audio file to transcribe
+    gcs_uri = f"gs://{os.getenv('GOOGLE_BUCKET')}/hello.mp3"
 
-    # Example 1 & 2. Transcribe Local Media File
-    # File Size: < 10mbs, length < 1 minute
-
-    ## Step 1. Load the media files
-    media_file_name_mp3 = './abc.mp3'
-
-    with open(media_file_name_mp3, 'rb') as f1:
-        byte_data_mp3 = f1.read()
-    audio_mp3 = speech.RecognitionAudio(content=byte_data_mp3)
-
-
-    ## Step 2. Configure Media Files Output
-    config_mp3 = speech.RecognitionConfig(
-        sample_rate_hertz=48000,
-        enable_automatic_punctuation=True,
-        language_code='zh-TW'
+    audio = speech.RecognitionAudio(uri=gcs_uri)
+    config = speech.RecognitionConfig(
+        encoding=speech.RecognitionConfig.AudioEncoding.WEBM_OPUS,
+        sample_rate_hertz=16000,
+        language_code="zh-TW",
     )
 
-    ## Step 3. Transcribing the RecognitionAudio objects
-    response_standard_mp3 = speech_client.recognize(
-        config=config_mp3,
-        audio=audio_mp3
-    )
+    operation = client.long_running_recognize(config=config, audio=audio)
 
-    print(response_standard_mp3)
+    print("Waiting for operation to complete...")
+    response = operation.result(timeout=90)
 
-    # # Example 3: Transcribing a long media file
-    # media_uri = 'gs://speech-to-text-media-files/Steve Job 2005 Commencement Speech.wav'
-    # long_audi_wav = speech.RecognitionAudio(uri=media_uri)
-    #
-    # config_wav_enhanced = speech.RecognitionConfig(
-    #     sample_rate_hertz=48000,
-    #     enable_automatic_punctuation=True,
-    #     language_code='en-US',
-    #     use_enhanced=True,
-    #     model='video'
-    # )
-    #
-    # operation = speech_client.long_running_recognize(
-    #     config=config_wav,
-    #     audio=long_audi_wav
-    # )
-    # response = operation.result(timeout=90)
-    # print(response)
-    #
-    # for result in response.results:
-    #     print(result.alternatives[0].transcript)
-    #     print(result.alternatives[0].confidence)
-    #     print()
+    # Each result is for a consecutive portion of the audio. Iterate through
+    # them to get the transcripts for the entire audio file.
+    for result in response.results:
+        # The first alternative is the most likely one for this portion.
+        print(u"Transcript: {}".format(result.alternatives[0].transcript))
+        print("Confidence: {}".format(result.alternatives[0].confidence))
